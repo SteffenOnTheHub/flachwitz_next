c3a932be21cca441ebed5fbb7a699476f45cf2cd
Steffen Kaiser <mr.photon@gmx.de>
2022-12-12 15:40:07 +0100

    added fallback: blocking to redirect to hopefully make page work on vercel

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ export async function getStaticProps() {
       destination: await getRandomJokeUrl(),
       permanent: false, // make this true if you want the redirect to be cached by the search engines and clients forever
     },
+    props: {
+      fallback: "blocking",
+    },
   };
 
 }
