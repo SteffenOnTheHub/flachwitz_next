05cad2d1648d676122a287920f3429223e33b932
Steffen Kaiser <mr.photon@gmx.de>
2022-12-12 10:51:11 +0100

    use next linkt instead of html link

--- components/navigationBar.js
+++ components/navigationBar.js
@@ -1,12 +1,16 @@
 import { IonIcon } from '../node_modules/@ionic/react' 
 import { arrowBack, arrowForward, scan } from 'ionicons/icons';
 
+import Link from 'next/link'
+
 function NavigationBarComponent(props) {    
     return (        
         <div id="joke-navigation-button-container">         
           <a className="icon-button" href="google.com"><IonIcon icon={arrowBack}/></a>
           <a className="icon-button" href="google.com"><IonIcon icon={scan}/></a>
-          <a className="icon-button" href={`/jokes/${encodeURIComponent(props.nextJokeData.id)}`}><IonIcon icon={arrowForward}/></a>
+          <Link href={props.nextJokeUrl} legacyBehavior>
+            <a className="icon-button"><IonIcon icon={arrowForward}/></a>
+          </Link>
         </div>     
     )
 }
--- pages/jokes/[id].js
+++ pages/jokes/[id].js
@@ -89,7 +89,7 @@ export async function getStaticProps({ params }) {
                 </Link>
           */}
 
-          <NavigationBar nextJokeData={props.nextJokeData} />
+          <NavigationBar nextJokeUrl={`/jokes/${encodeURIComponent(props.nextJokeData.id)}`} />
           {/* Likes 
           <div id="likes-container">
           */}
