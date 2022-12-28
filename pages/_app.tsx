fb71b8a501deb8ef95853ef9252b1f4bba9e7841
Steffen Kaiser <mr.photon@gmx.de>
2022-12-28 16:33:44 +0100

    migrated _app.js to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
