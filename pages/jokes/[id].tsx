f97b65f7ff7576d13af25bd5d2c3d50a79f5486c
Steffen Kaiser <mr.photon@gmx.de>
2023-01-01 19:18:37 +0100

    moved jokes into props again to make navigation work

--- pages/jokes/[id].tsx
+++ pages/jokes/[id].tsx
@@ -1,49 +1,92 @@
 import { getJokeData, getPreviousJokeData, getNextJokeData, getAllJokeIds, getAllJokesData } from '../../lib/jokes';
 import Layout from '../../components/layout';
 import NavigationBar from '../../components/navigationBar';
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { ParsedUrlQuery } from 'querystring';
 
-export async function getStaticProps({ params }) {
-    const jokeData = await getJokeData(params.id);
-    const previousJokeData = await getPreviousJokeData(params.id);
-    const nextJokeData = await getNextJokeData(params.id);
-    
-    const allJokes = getAllJokesData();
-    const randomJokeData = allJokes[Math.floor(Math.random() * allJokes.length)];
+type JokeData = {
+  id: string;
+  joke: string;
+};
 
+type PageProps = {
+  jokeData: JokeData;
+  previousJokeData: JokeData,
+  nextJokeData: JokeData,
+  randomJokeData: JokeData,
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string,
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const params = context.params!       // ! is a non-null assertion 
+  if(!params || !params.id){
+    console.log("no params")
     return {
-      props: {
-        jokeData,
-        previousJokeData,
-        nextJokeData,
-        randomJokeData,
-      },
+      props: { error: true },
     };
   }
+  
+  const jokeId:string = (params as ParsedUrlQuery).id! as string;
+  const jokeData = await getJokeData(jokeId);
+  const previousJokeData = await getPreviousJokeData(jokeId);
+  const nextJokeData = await getNextJokeData(jokeId);
+  
+  const allJokes = getAllJokesData();
+  const randomJokeData = allJokes[Math.floor(Math.random() * allJokes.length)];
 
-  export async function getStaticPaths() {
-    const paths = getAllJokeIds();
-    return {
-      paths,
-      fallback: false,
-    };
+  console.log("slug: ", jokeId)
+
+  return {
+    props: {
+      jokeData,
+      previousJokeData,
+      nextJokeData,
+      randomJokeData,
+    }
   }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllJokeIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 
-
+  export default function Joke( props : PageProps) {
+  
+  console.log("props: ", props)
   /*
-  export default function Joke({ jokeData }) {
-    return (
-      <Layout>
-       <div>
-        <h1>{jokeData.id}</h1>
-        <h1>{jokeData.joke}</h1>
-       </div>
-       </Layout>
-    );
-  }
+  export default function Joke({
+    jokeData
+  }: {
+    jokeData: {
+      id: string
+      joke: string
+    }
+  }) {
   */
 
-  export default function Joke(props) {
+ /*
+ export default function Joke({ jokeData }: { 
+      jokeData: {
+        id: string
+        joke: string
+      }
+    }) {
+  */
+  
+  
+  if(!props){
+      return;
+  }
+
+
     return (
       <Layout>
 
@@ -67,9 +110,8 @@ export async function getStaticProps({ params }) {
             previousJokeUrl={`/jokes/${encodeURIComponent(props.previousJokeData.id)}`} 
             nextJokeUrl={`/jokes/${encodeURIComponent(props.nextJokeData.id)}`} 
             randomJokeUrl={`/jokes/${encodeURIComponent(props.randomJokeData.id)}`} 
-
           />
-          
+
           {/* Likes 
           <div id="likes-container">
           */}
