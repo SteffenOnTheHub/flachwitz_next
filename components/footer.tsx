fbba9c491ce04b52ae44e3a93939e6229bc35088
Steffen Kaiser <mr.photon@gmx.de>
2023-01-09 17:45:50 +0100

    added about page

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,30 @@
+import Link from 'next/link'
+
 function FooterComponent() {    
 	return (        
         <footer id="footer">
+
+        <Link href="/about">About</Link>
+
+        {/*
         <div id="quelle">
             Quelle:
             <a href="https://github.com/derphilipp/Flachwitze"> 
-            {/* <ion-icon name="logo-github"></ion-icon> */}
+           
             github.com/derphilipp/Flachwitze
             </a> 
             
         </div>
+        */} 
+        {/*
         <div id="copyright">
             Copyright &copy;
             {new Date().getFullYear()} Flachwitze
         </div>
-        </footer>   
+        */}
+        
+        </footer>
+           
 	)
 }
 export default FooterComponent
---
+++ pages/about.tsx
@@ -0,0 +1,35 @@
+import Layout from '../components/layout';
+
+export default function About() {
+  
+    return (
+      <Layout>
+  
+  
+        <div id="about">
+            <p>
+                
+                Quelle der Witze: &nbsp;   
+                <a href="https://github.com/derphilipp/Flachwitze"> 
+                {/* <ion-icon name="logo-github"></ion-icon> */}
+                github.com/derphilipp/Flachwitze
+                </a> 
+            </p>
+
+            <p>
+                Source Code: &nbsp;
+                <a href="https://github.com/derphilipp/Flachwitze"> 
+                {/* <ion-icon name="logo-github"></ion-icon> */}
+                https://github.com/SteffenOnTheHub/flachwitz_next
+                </a> 
+            </p>
+
+            <p>
+                Für Feedback und Fragen erstellt einfach ein Issue in Github.
+            </p>
+            
+        </div>
+
+      </Layout>
+    );
+  }
