771df8d5ab18293b1e1ebdd9e37692591ac0e9a2
Steffen Kaiser <mr.photon@gmx.de>
2023-01-09 14:36:55 +0100

    added link to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 function HeaderComponent() {    
     return (        
         <header id="header">
+            <Link href="/">
             <h1> Flachwitze </h1>
+            </Link>
+            
             {/*
             <div id="logo"><img src="{% static 'logo.png' %}" alt="" /></div>
             <div id="top_menu">Home | Calendar | About | Contact</div>
